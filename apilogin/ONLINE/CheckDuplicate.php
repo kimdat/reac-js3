9d584e3be5614c5aeda6c7e7577eb78e97b57053
trần kim đạt <kimdat1999@gmail.com>
2023-03-15 04:18:24 +0700

    checkduplicate

---
+++ apilogin/ONLINE/CheckDuplicate.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Online;
+
+use Error;
+use Exception;
+use PDOException;
+use Throwable;
+
+//xóa những device trùng(update status D)
+class  CheckDuplicate
+{ //xóa 1 ip
+    function CheckDuplicate()
+    {
+        global $devicesDefine, $conn;
+        try {
+            $device = json_decode($_POST["device"]);
+            $device_ip = trim($device->ip);
+            $device_name = trim($device->deviceName);
+
+
+
+            $sql = "SELECT  " . $devicesDefine::COLUMN_DEVICES_IP . " FROM " . $devicesDefine::TABLE_DEVICES . " WHERE "
+                . $devicesDefine::COLUMN_DEVICES_STATUS . " <> 'D' AND ("
+                . $devicesDefine::COLUMN_DEVICES_IP . "=:ip OR "
+                . $devicesDefine::COLUMN_DEVICES_NAME . "=:name )";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':ip', $device_ip);
+            $stmt->bindParam(':name', $device_name);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                return json_encode(array("duplicate" => true));
+            }
+            return "";
+        } catch (Throwable $e) {
+            $currentFile = basename(__FILE__);
+            $currentFunction = __FUNCTION__;
+            throw new Error("Error $currentFunction in $currentFile ." . $e->getMessage());
+        }
+    }
+}
